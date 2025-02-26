aa4e0df88be9707f536319426a9f36e4f184f72f
Andrei Zavgorodnii <andrei.z@hadronlabs.org>
2025-02-26 19:49:02 +0000

    initial commit

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use neutron_oracle_history::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
